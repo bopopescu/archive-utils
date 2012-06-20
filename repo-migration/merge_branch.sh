057d4f52015aaaf992f19fc5bf06403a2a13af7c
Andrew Murray <andrew@lockerz.com>
2012-06-20 14:31:06 -0700

    Fix a small bug with branch migration

--- repo-migration/merge_branch.sh
+++ repo-migration/merge_branch.sh
@@ -16,7 +16,8 @@ cd ${dest_repo}
 git checkout master
 remote_name="${subdir}_old"
 git remote add -f ${remote_name} ${src_repo};
-git checkout -b ${branch_name}
+git branch ${branch_name}
+git checkout ${branch_name}
 git merge -s subtree --squash --no-commit ${remote_name}/${branch_name}
 git remote rm ${remote_name}
 
