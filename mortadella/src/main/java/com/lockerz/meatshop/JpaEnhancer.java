07152f333dfef384c85923c263ca5c61da7a6cb4
Brian Gebala <gebala@lockerz.com>
2013-01-22 10:20:10 -0800

    Renamed to JpaEnhancer to match the naming convention used by JPA.

--- mortadella/src/main/java/com/lockerz/meatshop/JpaEnhancer.java
+++ mortadella/src/main/java/com/lockerz/meatshop/JpaEnhancer.java
@@ -7,7 +7,7 @@ import org.apache.openjpa.lib.util.Options;
  * @author Brian Gebala
  * @version 1/22/13 9:23 AM
  */
-public class JpaEnhance {
+public class JpaEnhancer {
     public static void main(final String[] args) {
         PCEnhancer.run(new String[] {}, new Options());
     }
