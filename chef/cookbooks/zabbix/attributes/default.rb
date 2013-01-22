f9fca5bda009d39be98f8934250e35ac2e9415d1
steve <steve@lockerz.com>
2013-01-22 22:53:13 +0000

    Chef configuration for zabbix agent

---
+++ chef/cookbooks/zabbix/attributes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: zabbix
+# Attributes:: default
+
+default['zabbix']['agent']['install'] = true
+default['zabbix']['agent']['version'] = "2.0.4"
+default['zabbix']['agent']['servers'] = []
+default['zabbix']['agent']['servers_active'] = []
+default['zabbix']['agent']['hostname'] = node['fqdn']
+
+default['zabbix']['server']['install'] = false
+default['zabbix']['server']['version'] = "2.0.4"
+default['zabbix']['server']['branch'] = "ZABBIX%20Latest%20Stable"
+default['zabbix']['server']['dbname'] = "zabbix"
+default['zabbix']['server']['dbuser'] = "zabbix"
+default['zabbix']['server']['dbhost'] = "localhost"
+default['zabbix']['server']['dbport'] = "3306"
+ 
+default['zabbix']['install_dir'] = "/opt/zabbix"
+# default['zabbix']['etc_dir'] = "/etc/zabbix"
+default['zabbix']['etc_dir'] = "/usr/local/etc/zabbix"
+default['zabbix']['web_dir'] = "/opt/zabbix/web"
+default['zabbix']['external_dir'] = "/opt/zabbix/externalscripts"
+default['zabbix']['alert_dir'] = "/opt/zabbix/AlertScriptsPath"
+default['zabbix']['lock_dir'] = "/var/lock/subsys"
+default['zabbix']['src_dir'] = "/opt"
+default['zabbix']['log_dir'] = "/var/log/zabbix"
+default['zabbix']['run_dir'] = "/var/run/zabbix"
+
+default['zabbix']['login'] = "zabbix_agent"
+default['zabbix']['group'] = "zabbix_agent"
+default['zabbix']['uid'] = nil
+default['zabbix']['gid'] = nil
+default['zabbix']['home'] = '/opt/zabbix'
+default['zabbix']['shell'] = "/bin/bash"
---
+++ chef/cookbooks/zabbix/recipes/zabbix_client.rb
@@ -0,0 +1,97 @@
+
+if(node[:platform] == "ubuntu")
+
+  # Create zabbix group
+  group node['zabbix']['login'] do
+    gid node['zabbix']['gid']
+    if node['zabbix']['gid'].nil? 
+      action :nothing
+    else
+      action :create
+    end
+  end
+
+  # Create zabbix User
+  user node['zabbix']['login'] do
+    comment "zabbix User"
+    home node['zabbix']['install_dir']
+    shell node['zabbix']['shell']
+    uid node['zabbix']['uid']
+    gid node['zabbix']['gid'] 
+  end
+
+  # Define zabbix agent folders
+  root_dirs = [
+    # node['zabbix']['etc_dir'],
+    node['zabbix']['install_dir'],
+    "#{node['zabbix']['install_dir']}/bin",
+    "#{node['zabbix']['install_dir']}/sbin",
+    "#{node['zabbix']['install_dir']}/share"
+  ]
+
+  # Create root folders
+  root_dirs.each do |dir|
+    directory dir do
+      owner "zabbix_agent"
+      group "zabbix_agent"
+      mode "755"
+      recursive true
+    end
+  end
+
+  # Define zabbix log and run folders
+  zabbix_dirs = [
+    node['zabbix']['log_dir'],
+    node['zabbix']['run_dir']
+  ]
+
+  # Create zabbix folders
+  zabbix_dirs.each do |dir|
+    directory dir do
+      owner node['zabbix']['login']
+      group node['zabbix']['group']
+      mode "755"
+      recursive true
+      # Only execute this if zabbix can't write to it. This handles cases of
+      # dir being world writable (like /tmp)
+      # [ File.word_writable? doesn't appear until Ruby 1.9.x ]
+      not_if "su #{node['zabbix']['login']} -c \"test -d #{dir} && test -w #{dir}\""
+    end
+  end
+
+#   service "zabbix-client" do
+#       action [ :enable, :start ]
+#       supports :restart => true, :reload => true
+#       ignore_failure true
+#   end
+
+  cookbook_file "/opt/zabbix/sbin/zabbix_agentd" do
+     mode "0755"
+     owner "zabbix_agent"
+     group "zabbix_agent"
+     source "zabbix/zabbix_agentd"
+     # notifies :restart, resources(:service => "zabbix-client")
+  end
+
+  cookbook_file "/opt/zabbix/bin/zabbix_sender" do
+     mode "0755"
+     owner "zabbix_agent"
+     group "zabbix_agent"
+     source "zabbix/zabbix_sender"
+  end
+
+  cookbook_file "/opt/zabbix/bin/zabbix_get" do
+     mode "0755"
+     owner "zabbix_agent"
+     group "zabbix_agent"
+     source "zabbix/zabbix_get"
+  end
+
+  cookbook_file "/usr/local/etc/zabbix_agentd.conf" do
+     mode "0644"
+     owner "zabbix_agent"
+     group "zabbix_agent"
+     source "zabbix/zabbix_agentd.conf"
+     # notifies :restart, resources(:service => "zabbix-client")
+  end
+end
