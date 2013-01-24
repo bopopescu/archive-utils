3a3dd1a5a0fd8023082ddf37b211be9c1d52e619
James Baird <jbaird@lockerz.com>
2013-01-24 11:00:44 -0800

    Handlebars

--- mortadella/meatshop/src/main/java/com/lockerz/meatshop/service/shop/controller/ShopServiceController.java
+++ mortadella/meatshop/src/main/java/com/lockerz/meatshop/service/shop/controller/ShopServiceController.java
@@ -2,7 +2,11 @@ package com.lockerz.meatshop.service.shop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * @author Brian Gebala
@@ -15,5 +19,13 @@ public class ShopServiceController {
     @ResponseBody
     public String index() {
         return "Shops";
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test(final Map<String, Object> model,
+                       @RequestParam("name") final String name)
+    {
+        model.put("name", name);
+        return "shop/test";
     }
 }
